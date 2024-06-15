95603a51751afe1230bf7455ec0c42c2be3a80fd
Oussamabennabi <oussamabennabi0184@gmail.com>
2024-06-15 15:56:05 +0100

    init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,12 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  "parser": "@typescript-eslint/parser",
-  
+  // "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "project": true,
+
+  },
   "plugins": [
     "@typescript-eslint"
-    
   ],
   "extends": [
     "next/core-web-vitals",
@@ -15,7 +17,7 @@ const config = {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
-      "off",
+      "warn",
       {
         "prefer": "type-imports",
         "fixStyle": "inline-type-imports"
@@ -29,7 +31,7 @@ const config = {
     ],
     "@typescript-eslint/require-await": "off",
     "@typescript-eslint/no-misused-promises": [
-      "off",
+      "error",
       {
         "checksVoidReturn": {
           "attributes": false
